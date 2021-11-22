a3b725680508cda297cdf1f678f5323a6e5e75c5
Carlos <cmchamorro99@hotmail.com>
2021-11-22 04:22:01 -0600

    Initial commit

---
+++ src/main/kotlin/hn/edu/ujcv/baleadashermanas/BaleadasHermanasApplication.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.baleadashermanas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BaleadasHermanasApplication
+
+fun main(args: Array<String>) {
+	runApplication<BaleadasHermanasApplication>(*args)
+}
---
+++ src/test/kotlin/hn/edu/ujcv/baleadashermanas/BaleadasHermanasApplicationTests.kt
@@ -0,0 +1,13 @@
+package hn.edu.ujcv.baleadashermanas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BaleadasHermanasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
